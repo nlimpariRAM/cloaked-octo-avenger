55c4276ab5fe8d3242160637e486c5382fddcbaa
Nicholas Limparis <NickToGo>
2013-07-05 01:34:56 -0400

    i dicked up the newlines

--- helloworld.c
+++ helloworld.c
@@ -4,7 +4,7 @@ int main(void)
 {
     printf("Testing out some code\n");
 
-    printf("just dicking with some code");
+    printf("just dicking with some code\n");
 
     printf("more dicking around");
 }
